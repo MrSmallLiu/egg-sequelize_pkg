0450a2c93b520fd3b77531fe4a8edd4216173ce7
liuhang <1016817543@qq.com>
2019-07-25 09:14:32 +0800

    修复单元测试

--- test/fixtures/apps/sequelize_pkg-test/app/service/dcxt_cd.js
+++ test/fixtures/apps/sequelize_pkg-test/app/service/dcxt_cd.js
@@ -3,18 +3,18 @@
 const Service = require('egg').Service;
 
 class DcxtCdService extends Service {
-    async index() {
-        const { ctx } = this;
-        let result = {}
-        const datas = await ctx.model.DcxtCdb.findAndCountAll().catch(err => {
-            result.code = 0;
-            result.msg = '查询失败！';
-            result.data = err;
-            logger.error(new Error(err));
-        });
-        result.data = datas;
-        return result;
-    }
+  async index() {
+    const { ctx, logger } = this;
+    const result = {};
+    const datas = await ctx.model.DcxtCdb.findAndCountAll().catch(err => {
+      result.code = 0;
+      result.msg = '查询失败！';
+      result.data = err;
+      logger.error(new Error(err));
+    });
+    result.data = datas;
+    return result;
+  }
 }
 
 module.exports = DcxtCdService;
